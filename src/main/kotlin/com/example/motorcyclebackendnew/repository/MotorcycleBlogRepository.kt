157475b55665359ea35ff4ff05ff383f3c19b6a9
George Knapasjo <george.knapasjo@service.nsw.gov.au>
2021-07-03 16:36:54 +1000

    add query

--- src/main/kotlin/com/example/motorcyclebackendnew/repository/MotorcycleBlogRepository.kt
+++ src/main/kotlin/com/example/motorcyclebackendnew/repository/MotorcycleBlogRepository.kt
@@ -3,12 +3,15 @@ package com.example.motorcyclebackendnew.repository
 import com.example.motorcyclebackendnew.domain.MotorcycleBlog
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
 interface MotorcycleBlogRepository : CrudRepository<MotorcycleBlog, String> {
 
     @Query(value = "select * from blogs", nativeQuery = true)
     fun getBlogs(): List<MotorcycleBlog>
 
-//    @Query(value = "select * from vouchers where id", nativeQuery = true)
-//    fun getBlogs(): List<MotorcycleBlog>
+    @Query(value = "select * from vouchers where id = :id", nativeQuery = true)
+    fun getBlog(
+        @Param("id") id: String
+    ): List<MotorcycleBlog>
 }
